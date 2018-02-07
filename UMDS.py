229dcf85ba6bde50852c274a369d9aebdd75280f
Alan Hayes <ahayes@sei.com>
2018-02-07 13:51:05 -0500

    Find a file

--- UMDS.py
+++ UMDS.py
@@ -1,7 +1,8 @@
+import os
 
-
-# Create a configuration file /etc/odbcinst.ini
 file1 = '/etc/odbcinst.ini'
+file2 = 'libodbcpsqlS.so'
+path2 = '/usr/lib64/'
 
 
 def create_file(file1):
@@ -9,4 +10,12 @@ def create_file(file1):
     f.close()
 
 
+def find_file(path1, file1):
+    for root, dirs, files in os.walk(path1):
+        if file1 in files:
+            return os.path.join(root, file1)
+
+
 create_file(file1)
+found = find_file(path2, file2)
+print found
