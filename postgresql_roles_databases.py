1bb48af7f85f6194e393b3a92a010a232490b151
Alan Hayes <ahayes@sei.com>
2018-02-07 12:42:17 -0500

    updated password wrap

--- postgresql_roles_databases.py
+++ postgresql_roles_databases.py
@@ -2,18 +2,19 @@ import subprocess
 
 
 psql_db = 'umds'
+psql_pass = "'umds'"
 
 
-def psql_create_user(psql_db):
-    create_user = "CREATE USER %s WITH PASSWORD %s;" % (psql_db, psql_db)
+def psql_create_user(psql_db, psql_pass):
+    create_user = "CREATE USER %s WITH PASSWORD %s;" % (psql_db, psql_pass)
     args1 = ['sudo', '-u', 'postgres', 'psql', '-c', create_user]
     psql = subprocess.Popen(args1, stdout=subprocess.PIPE)
     output = psql.communicate()[0]
     return output
 
 
-def main(psql_db):
-    psql_create_user(psql_db)
+def main(psql_db, psql_pass):
+    psql_create_user(psql_db, psql_pass)
 
 
-main(psql_db)
+main(psql_db, psql_pass)
