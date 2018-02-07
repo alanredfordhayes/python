49cb24ee771145a3c1fac4c9eaafc36304c0e2a5
Alan Hayes <ahayes@sei.com>
2018-02-07 14:23:15 -0500

    Append without space

--- UMDS.py
+++ UMDS.py
@@ -26,13 +26,13 @@ def open_file_append(file1, append_line):
 def odbcinst_data(found2, found3):
     if type(found2) == str and type(found3) == str:
         append_line = """
-        [PostgreSQL]
-        Description=PostgreSQL ODBC driver (Unicode version)
-        Driver64=%s
-        Setup64=%s
-        Debug=0
-        CommLog=1
-        UsageCount=1
+[PostgreSQL]
+Description=PostgreSQL ODBC driver (Unicode version)
+Driver64=%s
+Setup64=%s
+Debug=0
+CommLog=1
+UsageCount=1
         """ % (found2, found3)
         return append_line
 
