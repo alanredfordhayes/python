a1702702e6484809eb29d76ed2b189a73986f065
Alan Hayes <ahayes@sei.com>
2018-02-08 08:42:23 -0500

    check symlink decision

--- UMDS.py
+++ UMDS.py
@@ -71,10 +71,9 @@ def edit_file(file, data):
 
 
 def check_symlink(dir1, dir2):
-    if dir1 == '/var/run/postgres/.s.PGSQL.5432':
-        print dir1
-    if dir2 == '/tmp/.s.PGSQL.5432':
-        print dir2
+    if (dir1 == '/var/run/postgresql/.s.PGSQL.5432'
+            and dir2 == '/tmp/.s.PGSQL.5432'):
+        print(dir1, dir2)
 
 
 create_file(file1)
